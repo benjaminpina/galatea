fd8c47de99bce5dfa93dd344af589e8145e30cab
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-03-14 13:53:10 -0600

    directory structure adapted for hexagonal architecture

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "galatea",
+		Title:  "Galatea",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
