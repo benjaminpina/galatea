3ebae1bacd2ae6419994a78de0b2cf0954ad7eef
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-04-06 15:38:50 -0600

    installed pinia

--- cmd/gui/frontend/src/main.ts
+++ cmd/gui/frontend/src/main.ts
@@ -3,11 +3,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import VueKonva from 'vue-konva';
 import App from './App.vue'
+import pinia from './stores'
 import router from './router';
 import './style.css';
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(ElementPlus)
 app.use(VueKonva)
 app.use(router)
