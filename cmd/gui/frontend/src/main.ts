2c6dba6ec6423f901d31c0ab0e49fbfcca79d7e1
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-04-06 15:46:41 -0600

    installed tanstack

--- cmd/gui/frontend/src/main.ts
+++ cmd/gui/frontend/src/main.ts
@@ -4,12 +4,14 @@ import 'element-plus/dist/index.css'
 import VueKonva from 'vue-konva';
 import App from './App.vue'
 import pinia from './stores'
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import router from './router';
 import './style.css';
 
 const app = createApp(App)
 
 app.use(pinia)
+app.use(VueQueryPlugin)
 app.use(ElementPlus)
 app.use(VueKonva)
 app.use(router)
