0574f8bdc133af796b379217a2e1e61c4c6566ec
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2025-04-06 19:03:27 -0600

    installed element icons

--- cmd/gui/frontend/src/main.ts
+++ cmd/gui/frontend/src/main.ts
@@ -1,5 +1,6 @@
 import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import VueKonva from 'vue-konva';
 import App from './App.vue'
@@ -15,5 +16,10 @@ app.use(VueQueryPlugin)
 app.use(ElementPlus)
 app.use(VueKonva)
 app.use(router)
+
+// Register ElementPlus icons
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.mount('#app')
